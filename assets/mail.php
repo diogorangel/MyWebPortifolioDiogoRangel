89b287c80e80c3decf93193158d94f795a533692
Rangel Dos Santos <drangeld@minsait.com>
2025-01-17 23:14:37 -0300

    versao 0.2 website updated

--- assets/mail.php
+++ assets/mail.php
@@ -1,126 +1,43 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form fields and sanitize input
+    $name = isset($_POST["name"]) ? htmlspecialchars(trim($_POST["name"])) : null;
+    $email = isset($_POST["email"]) ? filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL) : null;
+    $message = isset($_POST["message"]) ? htmlspecialchars(trim($_POST["message"])) : null;
 
-
-
-    // Only process POST reqeusts.
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        // Get the form fields and remove MORALspace.
-
-        // NAME
-
-        if(isset($_POST["name"])){
-            $name = trim($_POST["name"]);
-        }else{
-            $name = "hi";
-        }
-
-        // EMAIL
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-
-        // IF MESSAGE SET
-        if(isset($_POST["message"])){
-            $message = trim($_POST["message"]);
-        }else{
-            $message = "hi";
-        }
-
-
-        // Check that data was sent to the mailer.
-
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-            // Set a 400 (bad request) response code and exit.
-
-            http_response_code(400);
-
-            echo "Please complete the form and try again.";
-
-            exit;
-
-        }
-
-
-
-        // Set the recipient email address.
-
-        // FIXME: Update this to your desired email address.
-
-        $recipient = "mun599@gmail.com";
-
-
-
-        // Set the email subject.
-
-        $sender = "New contact from $name";
-
-
-
-        //Email Header
-
-        $head = " /// Webtend Omio HTML \\\ ";
-
-
-
-        // Build the email content.
-
-        $email_content = "$head\n\n\n";
-
-        if(isset($_POST["name"])){
-            $email_content .= "Name: $name\n";
-        }
-
-        
-        $email_content .= "Email: $email\n\n";
-
-
-        // IF SET Message
-        if(isset($_POST["message"])){
-            $email_content .= "Message:\n$message\n";
-        }
-
-
-        // Build the email headers.
-
-        if(isset($_POST["name"])){
-            $email_headers = "From: $name <$email>";
-        }
-
-
-        // Send the email.
-
-        if (mail($recipient, $sender, $email_content, $email_headers)) {
-
-            // Set a 200 (okay) response code.
-
-            http_response_code(200);
-
-            echo "Thank You! Your message has been sent.";
-
-        } else {
-
-            // Set a 500 (internal server error) response code.
-
-            http_response_code(500);
-
-            echo "Oops! Something went wrong and we couldn't send your message.";
-
-        }
-
-
-
-    } else {
-
-        // Not a POST request, set a 403 (forbidden) response code.
-
-        http_response_code(403);
-
-        echo "There was a problem with your submission, please try again.";
-
+    // Validate required fields
+    if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(400);
+        echo "Please complete all fields correctly and try again.";
+        exit;
     }
 
+    // Set the recipient email address
+    $recipient = "diodam.rangel@gmail.com"; // Replace with your email address
 
+    // Set the email subject
+    $subject = "New contact from $name";
 
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers
+    $email_headers = "From: $name <$email>\r\n";
+    $email_headers .= "Reply-To: $email\r\n";
+
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank you! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+} else {
+    // Not a POST request
+    http_response_code(403);
+    echo "There was a problem with your submission. Please try again.";
+}
 ?>
-
